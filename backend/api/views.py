98d88a746bf58fc35f160742bdf352d5335d74bf
RaoShailendrasinh <raoshailendra18@gmail.com>
2026-03-06 20:04:50 +0530

    Writing Create, Read & Delete Views

--- backend/api/views.py
+++ backend/api/views.py
@@ -2,8 +2,31 @@ from django.shortcuts import render
 from django.contrib.auth.models import User
 from rest_framework import generics
 from rest_framework.permissions import IsAuthenticated, AllowAny
-from .serializers import UserSerializer
+from .serializers import UserSerializer, NoteSerializer
+from .models import Note
 
+class NoteListCreateView(generics.ListCreateAPIView):
+    serializer_class = NoteSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        user = self.request.user
+        return Note.objects.filter(user=user)
+        
+    def perform_create(self, serializer):
+        if serializer.is_valid(): 
+            serializer.save(user=self.request.user)
+        else:
+            print(serializer.errors)
+            
+
+class NoteDeleteView(generics.DestroyAPIView):
+    serializer_class = NoteSerializer
+    permission_classes = [IsAuthenticated]
+    
+    def get_queryset(self):
+        user = self.request.user
+        return Note.objects.filter(user=user)
 
 
 
--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -12,5 +12,5 @@ urlpatterns = [
     path('api/token/', TokenObtainPairView.as_view(), name='token'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('api-auth/', include('rest_framework.urls')),
+    path('api/', include('api.urls')),
 ]
-    
